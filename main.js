f31fc188e2ba390c34d00a94b016ddfe24deb21d
SirCagus <141450259+SirCagus@users.noreply.github.com>
2024-10-09 12:02:31 -0300

    wer

---
+++ main.js
@@ -0,0 +1,11 @@
+function criaCartao(){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `<div class="conteudoCartao">
+    <h3>Categoria</h3>
+    <div class="perguntaCartao">
+        <p>Pergunta</p>
+    </div>
+</div>`
+}
