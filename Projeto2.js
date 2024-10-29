14a922169dd6161446d7cae3809cedbcdca33248
SirCagus <141450259+SirCagus@users.noreply.github.com>
2024-10-29 08:37:08 -0300

    sd

--- Projeto2.js
+++ Projeto2.js
@@ -2,7 +2,7 @@ function criaCartao(Categoria, Pergunta, Resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = `<div class="conteudoCartao" onClick="mostraCartao(this)>
+    cartao.innerHTML = `<div class="conteudoCartao" onclick="mostraCartao(this)>
     <h3>${Categoria}</h3>
     <div class="perguntaCartao">
         <p>${Pergunta}</p>
@@ -16,7 +16,7 @@ container.appendChild(cartao);
 
 
 function mostraCartao(card) {
-const Resposta = card.querySelector(".respostaCartao");
+const Resposta = card.querySelector('.respostaCartao');
 if (Resposta.style.display === 'none')
    (Resposta.style.display = 'block')
 
