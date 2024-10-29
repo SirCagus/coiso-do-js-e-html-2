ad91cb665db3344d3a8cf36543616f82c5df20cb
SirCagus <141450259+SirCagus@users.noreply.github.com>
2024-10-29 08:43:47 -0300

    sd

--- Projeto2.js
+++ Projeto2.js
@@ -1,26 +1,23 @@
-function criaCartao(categoria, pergunta, resposta) {
+function criaCartao(Categoria, Pergunta, Resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = `
-        <div class="conteudo-cartao" onclick="mostraResposta(this)">
-            <h3>${categoria}</h3>
-            <div class="pergunta-cartao">
-                <p>${pergunta}</p>
-            </div>
-            <div class="resposta-cartao" style="display: none;">
-                <p>${resposta}</p>
-            </div>
-        </div>
-    `;
-    container.appendChild(cartao);
+    cartao.innerHTML = `<div class="conteudoCartao" onclick="mostraCartao(this)>
+    <h3>${Categoria}</h3>
+    <div class="perguntaCartao">
+        <p>${Pergunta}</p>
+    </div>
+    <div class="respostaCartao">
+        <p>${Resposta}</p>
+    </div>
+</div>`
+container.appendChild(cartao);
 }
 
-function mostraResposta(card) {
-    const resposta = card.querySelector('.resposta-cartao'); // Adicione o ponto para a classe
-    if (resposta.style.display === 'none' || resposta.style.display === '') { // Adicione condição para verificar o estado
-        resposta.style.display = 'block';
-    } else {
-        resposta.style.display = 'none';
-    }
-}
+
+function mostraCartao(card) {
+const Resposta = card.querySelector('.respostaCartao');
+if (Resposta.style.display === 'none')
+   Resposta.style.display = 'block' ;
+
+else {Resposta.style.display = 'none'}}
